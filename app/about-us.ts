71f9d12c68bfa365f4e5fa11ffac3ab45e8671a4
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2016-03-13 19:14:30 +0530

    dont add router to shims

---
+++ app/about-us.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+@Component({
+	template: `
+    <h2>About Us</h2>
+    <p>Developed by Saptarshi Chatterjee</p>`
+})
+export class AboutUs { }
---
+++ app/mission.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+@Component({
+	template: `
+    <h2>Mission</h2>
+    <p>Explain the basics of Angular2</p>`
+})
+export class Mission { }
