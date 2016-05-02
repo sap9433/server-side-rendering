d131d77a022245895b27bfb4c5dd7f410f0f46a9
Sapy <saptarshichatterjee1@gmail.com>
2016-05-02 20:01:03 +0530

    http client working

--- app/httpClient/client.ts
+++ app/httpClient/client.ts
@@ -2,12 +2,11 @@ import {Component} from 'angular2/core';
 import { HTTP_PROVIDERS }    from 'angular2/http';
 import {DataService} from './dataService.ts';
 
-
 @Component({
 	template: `
-    <h2>About Us</h2>
-    <p>Developed by App2Fusion.</p>
-    <pre> {{ data }} </pre>`,
+    <h2>List of Github Repositories by Ng2</h2>
+    <p *ngFor="let datum of data; let i = index"> {{i+1}}. - {{datum.full_name}}</p>
+    <p *ngIf="errorMessage"> Error : {{errorMessage}}</p>`,
 	providers: [HTTP_PROVIDERS, DataService]
 })
 export class HttpClient { 
@@ -18,7 +17,8 @@ export class HttpClient {
 	fetchData() {
 		this._dataService.getData()
 			.subscribe(
-			data => this.data = data,
-			error => this.errorMessage = <any>error);
+				data => this.data = data,
+				err => this.errorMessage = err
+			);
 	}
 }
